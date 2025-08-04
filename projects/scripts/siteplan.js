5b7cd96b4744d6389bc6ac143c543b7c3fa956da
UgochiGoodness <ugochigoodness08@gmail.com>
2025-08-04 16:01:52 -0700

    update

--- projects/scripts/siteplan.js
+++ projects/scripts/siteplan.js
@@ -10,3 +10,13 @@ firstPara.innerHTML = `&copy; ${currentYear} ♦ Ugochi Goodness Ugorji ♦ Nige
 const lastModified = document.lastModified;
 const secondPara = footer.querySelectorAll('p')[1]; // second <p>
 secondPara.textContent = `Last Modified: ${lastModified}`;
+
+// Toggle mobile nav
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("menuToggle");
+    const nav = document.getElementById("navbar");
+
+    toggleButton.addEventListener("click", () => {
+        nav.classList.toggle("show");
+    });
+});
